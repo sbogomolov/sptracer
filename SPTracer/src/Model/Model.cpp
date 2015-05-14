038fbf4a0f5ceeef56b8bc3a015c1ff2a0ff3c95
Sergii Bogomolov <bogomolov.sv@gmail.com>
2015-05-14 15:44:50 +0200

    Code cleanup

--- SPTracer/src/Model/Model.cpp
+++ SPTracer/src/Model/Model.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "../Intersection.h"
 #include "../Ray.h"
 #include "../Util.h"
@@ -15,66 +16,26 @@ namespace SPTracer
 
 	bool Model::Intersect(const Ray& ray, Intersection& intersection) const
 	{
-		bool found = false;
-		Intersection current;
-		// bool foundEmissive = false;		// used for more complicated condition
+		// set initial intersection distance to max possible,
+		// so that any intersection will be closer than that
+		intersection.distance = std::numeric_limits<float>::infinity();
+
+		Intersection newIntersection;
 
 		for (const auto& o : objects_)
 		{
-			bool success = o->Intersect(ray, current);
+			// find new intersection
+			bool success = o->Intersect(ray, newIntersection);
 			
-			// no intersection
-			if (!success)
+			// check if new intersection is closer
+			if (success && (newIntersection.distance < intersection.distance))
 			{
-				continue;
+				intersection = newIntersection;
+				intersection.object = o;
 			}
-
-			/////////////////////////////////////////////////////////////////////////////////////
-			// NOTE: More complicated condition for badly positioned light sources.
-			// This is more complicated condition. It can be used for models where light sourcse
-			// have some surfaces behind them. Normally, there should be some distance between a 
-			// surface and light source, in which case a simplified condition can be used
-			/////////////////////////////////////////////////////////////////////////////////////
-
-			//// if some intersection was already found
-			//if (found)
-			//{
-			//	// difference between distances
-			//	float diff = std::abs(current.distance - intersection.distance);
-
-			//	// distance to new intersection is different from distance to old intersection
-			//	if (diff > Util::Eps)
-			//	{
-			//		// new intersection is not closer that the previous one
-			//		if (current.distance > intersection.distance)
-			//		{
-			//			continue;
-			//		}
-			//	}
-			//	else
-			//	{
-			//		// same distance to intersection
-			//		if (foundEmissive)
-			//		{
-			//			// previous intersection was with emissive material, so it has higher priority
-			//			continue;
-			//		}
-			//	}
-			//}
-
-			// new intersection is not closer
-			if (found && (current.distance > intersection.distance))
-			{
-				continue;
-			}
-
-			found = true;
-			intersection = current;
-			intersection.object = o;
-			// foundEmissive = o->IsEmissive();		// used for more complicated condition
 		}
 
-		return found;
+		return intersection.distance < std::numeric_limits<float>::infinity();
 	}
 
 }
