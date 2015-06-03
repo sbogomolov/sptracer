f887498e8eea93426af4ce99e5b1a4d2c226c625
Sergii Bogomolov <bogomolov.sv@gmail.com>
2015-06-03 10:38:41 +0200

    Changed ray-box intersection algorithm to reference slabs method

--- SPTracer/src/SPTracer/Primitive/Box.cpp
+++ SPTracer/src/SPTracer/Primitive/Box.cpp
@@ -31,31 +31,59 @@ namespace SPTracer
 		return 2.0f * (d[0] * d[1] + d[1] * d[2] + d[2] * d[0]);
 	}
 
-	bool Box::Intersect(const Ray& ray, const Vec3& invDirection)
+	bool Box::Intersect(const Ray& ray, const Vec3& invDirection, Vec3& near, Vec3& far)
 	{
 		//
-		// Kay and Kayjia "slabs" method, optimized by Tavian Barnes
-		// http://tavianator.com/fast-branchless-raybounding-box-intersections-part-2-nans/
+		// Kay and Kayjia "slabs" method
 		//
 
-		// X planes
-		float t1 = (min_[0] - ray.origin[0]) * invDirection[0];
-		float t2 = (max_[0] - ray.origin[0]) * invDirection[0];
+		float tnear = std::numeric_limits<float>::min();
+		float tfar = std::numeric_limits<float>::max();
 
-		float tmin = std::min(t1, t2);
-		float tmax = std::max(t1, t2);
-
-		// Y and Z planes
-		for (size_t i = 1; i < 3; i++)
+		for (size_t i = 0; i < 3; i++)
 		{
-			t1 = (min_[i] - ray.origin[i]) * invDirection[i];
-			t2 = (max_[i] - ray.origin[i]) * invDirection[i];
+			// check if ray is parallel to the plane
+			if (std::abs(ray.direction[i]) < Util::Eps)
+			{
+				// check if ray is in between the slabs
+				if ((ray.origin[i] < min_[i]) || (ray.origin[i] > max_[i]))
+				{
+					// ray is outside
+					return false;
+				}
 
-			tmin = std::max(std::min(t1, t2), tmin);
-			tmax = std::min(std::max(t1, t2), tmax);
+				// ray is in between the slabs, check other planes
+				continue;
+			}
+
+			// compute the intersection distances to the planes
+			float t1 = (min_[i] - ray.origin[i]) * invDirection[i];
+			float t2 = (max_[i] - ray.origin[i]) * invDirection[i];
+			
+			// make sure that t1 <= t2
+			if (t1 > t2)
+			{
+				std::swap(t1, t2);
+			}
+
+			// update tnear and tfar
+			tnear = std::max(tnear, t1);
+			tfar = std::min(tfar, t2);
+
+			// check that we're still inside the box:
+			//   tnear > tfar  means that ray misses the box
+			//   tfar < 0.0f   means that the box is behind the ray origin
+			if ((tnear > tfar) || (tfar < 0.0f))
+			{
+				return false;
+			}
 		}
 
-		return tmax > std::max(tmin, 0.0f);
+		// we have intersection, let's compute the intersection points
+		near = ray.origin + ray.direction * tnear;
+		far = ray.origin + ray.direction * tfar;
+
+		return true;
 	}
 
 	bool Box::IsPlanar(unsigned char dimension) const
